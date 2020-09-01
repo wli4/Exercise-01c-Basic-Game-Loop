ee89a69fca3a10bab624a9d78ad47826434191f6
Jason Francis <jarfranc@indiana.edu>
2020-09-01 13:14:46 -0400

    Initial commit

--- main.py
+++ main.py
@@ -4,20 +4,50 @@ assert sys.version_info >= (3,8), "This script requires at least Python 3.8"
 
 def load(l):
     f = open(os.path.join(sys.path[0], l))
-    data=f.read()
-    return json.loads(data)
-    
-    
+    data = f.read()
+    j = json.loads(data)
+    return j
+
+def find_passage(game_desc, pid):
+    for p in game_desc["passages"]:
+        if p["pid"] == pid:
+            return p
+    return {}
 
 
 
+# ------------------------------------------------------
 
+def update(current, game_desc, choice):
+    if current == "":
+        return current
+    return current
 
+def render(current):
+    pass
 
+def get_input(current):
+    choice = input("What would you like to do? (type quit to exit) ")
+    choice = choice.lower()
+    if choice in ["quit","q","exit"]:
+        return "quit"
+    return choice
+
+# ------------------------------------------------------
 
 def main():
     game_desc = load("adventure.json")
-    print(game_desc)
+    current = find_passage(game_desc, game_desc["startnode"])
+    choice = ""
+
+    while choice != "quit" and current != {}:
+        current = update(current, game_desc, choice)
+        render(current)
+        choice = get_input(current)
+
+    print("Thanks for playing!")
+
+
 
 
 if __name__ == "__main__":
